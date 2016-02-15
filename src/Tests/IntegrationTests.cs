b0fe0c646b33c5b303ecc683beef2ff8bce7587f
Mihai Mogosanu <mike@sapiensworks.com>
2016-02-15 13:22:50 -0500

    added tests project

---
+++ src/Tests/IntegrationTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Tests
+{
+    public class IntegrationTests
+    {
+        public IntegrationTests()
+        {
+
+        }
+
+        [Fact]
+        public void testName()
+        {
+            
+        }
+
+    }
+}
