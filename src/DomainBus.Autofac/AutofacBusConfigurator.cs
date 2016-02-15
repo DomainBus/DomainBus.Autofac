c01e0c420b51f15619939bb620a3b5c605b8bd05
Mihai Mogosanu <mike@sapiensworks.com>
2016-02-15 13:18:06 -0500

    updated implementation

---
+++ src/DomainBus.Autofac/AutofacBusConfigurator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Autofac;
+using CavemanTools.Infrastructure;
+using DomainBus.Configuration;
+
+namespace DomainBus.Autofac
+{
+    
+ public class AutofacBusConfigurator : AbstractBusBuilder
+    {
+        private ILifetimeScope _scope;
+        private ContainerBuilder _cb;
+
+        public AutofacBusConfigurator():this(null)
+        {
+            _cb = new ContainerBuilder();
+        }
+
+        public AutofacBusConfigurator(ILifetimeScope scope)
+        {
+            _scope = scope;
+        }
+        protected override void RegisterSingletonInstance<T>(T instance)
+        {
+            _cb.Register(c => instance).AsSelf().AsImplementedInterfaces().SingleInstance();
+        }
+
+     protected override void Register(IEnumerable<Type> types)
+     {
+            _cb.RegisterTypes(types.ToArray()).AsSelf();
+        }
+
+     protected override void RegisterInstanceFactory<T>(Func<T> instance)
+     {
+         _cb.Register(c => instance()).As<T>();
+     }
+
+     protected override void ContainerConfigurationCompleted()
+     {
+        _cb.Update(_scope.ComponentRegistry);
+     }
+
+     protected override IContainerScope BuildContainerScope()
+     {
+            if (_scope == null)
+            {
+                _scope = _cb.Build();
+            }
+            else
+            {
+                _cb.Update(_scope.ComponentRegistry);
+            }
+            
+            return new AutofacWrapper(_scope);
+        }
+
+   
+
+     
+      
+    }
+}
---
+++ src/DomainBus.Autofac/AutofacWrapper.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using Autofac;
+using CavemanTools.Infrastructure;
+
+namespace DomainBus.Autofac
+{
+    internal class AutofacWrapper : IContainerScope
+    {
+        private readonly ILifetimeScope _container;
+
+        public AutofacWrapper(ILifetimeScope container)
+        {
+            _container = container;
+        }
+
+        public object Resolve(Type type)
+        {
+            return _container.Resolve(type);
+        }
+
+        public T Resolve<T>()
+        {
+            return _container.Resolve<T>();
+        }
+
+        /// <summary>
+        /// If type is not registered return null
+        /// </summary>
+        /// <param name="type"/>
+        /// <returns/>
+        public object ResolveOptional(Type type)
+        {
+            return _container.ResolveOptional(type);
+        }
+
+        public T ResolveOptional<T>() where T : class
+        {
+            return _container.ResolveOptional<T>();
+        }
+
+        public IEnumerable<T> GetServices<T>()
+        {
+            var rez= _container.Resolve<IEnumerable<T>>();
+            if (rez == null)
+            {
+                return new T[0];
+            }
+            return rez;
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            _container.Dispose();
+        }
+
+        public IContainerScope BeginLifetimeScope()
+        {
+            return new AutofacWrapper(_container.BeginLifetimeScope());
+        }
+    }
+
+
+}
