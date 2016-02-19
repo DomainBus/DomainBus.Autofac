c22d4836ed80c926fc59b6e357a2316105b3dc5d
Mihai Mogosanu <mike@sapiensworks.com>
2016-02-19 16:47:06 -0500

    updated build files

--- build/settings.fsx
+++ build/settings.fsx
@@ -5,6 +5,9 @@ let projName="DomainBus.Autofac"
 let projDir= "..\src" @@ projName
 let testDir="..\src" @@ "Tests"
 
+let testOnCore=true
+let additionalPack=[]
+
 let localNugetRepo="E:/Libs/nuget"
 let nugetExeDir="tools"
 
